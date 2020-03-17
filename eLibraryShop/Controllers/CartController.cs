0c691dd300fbc5526bde6312bbf3855f39db6710
SilverSlasher <krzysanisk@gmail.com>
2020-03-17 10:50:07 +0100

    Fixed cart redirecting. Added "loading and adding" script. Many smaller changes

--- eLibraryShop/Controllers/CartController.cs
+++ eLibraryShop/Controllers/CartController.cs
@@ -35,30 +35,9 @@ namespace eLibraryShop.Controllers
         // GET  /cart/add/id
         public async Task<IActionResult> Add(int id)
         {
-           Book book = await context.Books.FirstOrDefaultAsync(x => x.Id == id);
+            Book book = await context.Books.FirstOrDefaultAsync(x => x.Id == id);
 
-           List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart") ?? new List<CartItem>();
-
-           CartItem cartItem = cart.Where(x => x.BookId == id).FirstOrDefault();
-
-           if (cartItem == null)
-           {
-                cart.Add(new CartItem(book));
-           }
-           else
-           {
-               cartItem.Quantity += 1;
-           }
-
-           HttpContext.Session.SetJson("Cart",cart);
-
-           return RedirectToAction("Index");
-        }
-
-        // GET  /cart/decrease/id
-        public async Task<IActionResult> Decrease(int id)
-        {
-            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart");
+            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart") ?? new List<CartItem>();
 
             CartItem cartItem = cart.Where(x => x.BookId == id).FirstOrDefault();
 
@@ -73,7 +52,71 @@ namespace eLibraryShop.Controllers
 
             HttpContext.Session.SetJson("Cart", cart);
 
+            if (HttpContext.Request.Headers["X-Requested-With"] != "XMLHttpRequest")
+            {
+                return RedirectToAction("Index");
+            }
+
+            return ViewComponent("SmallCart");
+        }
+
+        // GET  /cart/decrease/id
+        public IActionResult Decrease(int id)
+        {
+            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart");
+
+            CartItem cartItem = cart.Where(x => x.BookId == id).FirstOrDefault();
+
+            if (cartItem.Quantity > 1)
+            {
+                --cartItem.Quantity;
+            }
+            else
+            {
+                cart.RemoveAll(x => x.BookId == id);
+            }
+
+
+            if (cart.Count == 0)
+            {
+                HttpContext.Session.Remove("Cart");
+            }
+            else
+            {
+                HttpContext.Session.SetJson("Cart", cart);
+            }
+
             return RedirectToAction("Index");
+        }
+
+
+        // GET  /cart/remove/id
+        public IActionResult Remove(int id)
+        {
+            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart");
+
+            cart.RemoveAll(x => x.BookId == id);
+
+
+            if (cart.Count == 0)
+            {
+                HttpContext.Session.Remove("Cart");
+            }
+            else
+            {
+                HttpContext.Session.SetJson("Cart", cart);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+        // GET  /cart/clear
+        public IActionResult Clear(int id)
+        {
+            HttpContext.Session.Remove("Cart");
+
+            return Redirect(Request.Headers["Referer"].ToString());
         }
     }
 }
---
+++ eLibraryShop/Infrastructure/SmallCartViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eLibraryShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eLibraryShop.Infrastructure
+{
+    public class SmallCartViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart");
+
+            SmallCartViewModel smallCartVM = null;
+
+            if (cart == null || cart.Count == 0)
+            {
+                smallCartVM = null;
+            }
+            else
+            {
+                smallCartVM = new SmallCartViewModel
+                {
+                    NumberOfItems = cart.Sum(x => x.Quantity),
+                    TotalAmount = cart.Sum(x => x.Total)
+                };
+            }
+
+            return View(smallCartVM);
+        }
+    }
+}
